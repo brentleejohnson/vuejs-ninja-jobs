029c2ad4a617e44873d26da0e27b0178219ab283
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-19 09:16:18 +0200

    Route basics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi odit
+      distinctio dolore repellendus pariatur. Nisi tempore fugiat velit eos.
+      Quasi consequatur fugiat tenetur temporibus cumque, qui veniam ab iste,
+      aut nemo harum, quas enim dolor! Ex harum sunt repellendus aspernatur
+      quisquam voluptas adipisci ad tempora doloribus, exercitationem aliquam
+      distinctio non voluptate unde assumenda tenetur nulla libero reprehenderit
+      fuga molestias nam minus quibusdam impedit! Incidunt nobis quam porro
+      voluptatum aliquam eos voluptatem placeat libero voluptates, laudantium
+      odio voluptatibus optio sapiente id, necessitatibus vitae sed
+      exercitationem officiis amet odit veniam nihil, temporibus sunt.
+      Asperiores laborum laudantium sed corrupti molestiae molestias suscipit
+      qui!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi odit
+      distinctio dolore repellendus pariatur. Nisi tempore fugiat velit eos.
+      Quasi consequatur fugiat tenetur temporibus cumque, qui veniam ab iste,
+      aut nemo harum, quas enim dolor! Ex harum sunt repellendus aspernatur
+      quisquam voluptas adipisci ad tempora doloribus, exercitationem aliquam
+      distinctio non voluptate unde assumenda tenetur nulla libero reprehenderit
+      fuga molestias nam minus quibusdam impedit! Incidunt nobis quam porro
+      voluptatum aliquam eos voluptatem placeat libero voluptates, laudantium
+      odio voluptatibus optio sapiente id, necessitatibus vitae sed
+      exercitationem officiis amet odit veniam nihil, temporibus sunt.
+      Asperiores laborum laudantium sed corrupti molestiae molestias suscipit
+      qui!
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi odit
+      distinctio dolore repellendus pariatur. Nisi tempore fugiat velit eos.
+      Quasi consequatur fugiat tenetur temporibus cumque, qui veniam ab iste,
+      aut nemo harum, quas enim dolor! Ex harum sunt repellendus aspernatur
+      quisquam voluptas adipisci ad tempora doloribus, exercitationem aliquam
+      distinctio non voluptate unde assumenda tenetur nulla libero reprehenderit
+      fuga molestias nam minus quibusdam impedit! Incidunt nobis quam porro
+      voluptatum aliquam eos voluptatem placeat libero voluptates, laudantium
+      odio voluptatibus optio sapiente id, necessitatibus vitae sed
+      exercitationem officiis amet odit veniam nihil, temporibus sunt.
+      Asperiores laborum laudantium sed corrupti molestiae molestias suscipit
+      qui!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi odit
+      distinctio dolore repellendus pariatur. Nisi tempore fugiat velit eos.
+      Quasi consequatur fugiat tenetur temporibus cumque, qui veniam ab iste,
+      aut nemo harum, quas enim dolor! Ex harum sunt repellendus aspernatur
+      quisquam voluptas adipisci ad tempora doloribus, exercitationem aliquam
+      distinctio non voluptate unde assumenda tenetur nulla libero reprehenderit
+      fuga molestias nam minus quibusdam impedit! Incidunt nobis quam porro
+      voluptatum aliquam eos voluptatem placeat libero voluptates, laudantium
+      odio voluptatibus optio sapiente id, necessitatibus vitae sed
+      exercitationem officiis amet odit veniam nihil, temporibus sunt.
+      Asperiores laborum laudantium sed corrupti molestiae molestias suscipit
+      qui!
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
